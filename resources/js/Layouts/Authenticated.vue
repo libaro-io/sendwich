15f0718930a3f6a4c5f7c29cf68437ed70613a3b
Jennis Vanhaeke <jennis@libaro.be>
2022-07-06 17:26:14 +0200

    Now logo

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -21,7 +21,6 @@ const showingNavigationDropdown = ref(false);
     <div>
         <div class="min-h-screen bg-gray-100">
             <nav class="bg-nav shadow-md">
-                <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
