67354c687c4b28a87c2589861b5cf30eda5443b7
Jennis Vanhaeke <jennis@libaro.be>
2022-07-07 13:11:38 +0200

    bg fix

--- resources/js/Components/DeptList.vue
+++ resources/js/Components/DeptList.vue
@@ -3,7 +3,7 @@
         <div class="px-4 py-5 sm:p-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">The Blacklist</h3>
             <div class="mt-5">
-                <div v-for="user in users" :class="user.dept > 0 ? 'bg-green-50': 'bg-red-50'" class="rounded-md mb-1 bg-gray-50 px-6 py-5 sm:flex sm:items-start sm:justify-between">
+                <div v-for="user in users" :class="user.dept > 0 ? 'bg-green-50': 'bg-red-50'" class="rounded-md mb-1 px-6 py-5 sm:flex sm:items-start sm:justify-between">
                     <div class="sm:flex sm:items-start">
                         <div class="mt-3 sm:mt-0 sm:ml-4">
                             <div class="text-sm font-medium text-gray-900">{{ user.name }}</div>
