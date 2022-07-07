6a8b80d7e67b0032c1a345f7321db77ba8fa6d79
Jennis Vanhaeke <jennis@libaro.be>
2022-07-07 13:09:23 +0200

    header update

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -12,8 +12,8 @@ const showingNavigationDropdown = ref(false);
 <style>
 .bg-nav{
     background: #a8ff78;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #78ffd6, #a8ff78);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #78ffd6, #a8ff78); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: -webkit-linear-gradient(to right, #78ffd6, #7fbeab);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #78ffd6, #7fbeab); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 }
 </style>
