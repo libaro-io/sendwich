cbfbe0568276935b515ebc8ea59dc604a92ca16a
Jennis Vanhaeke <jennis@libaro.be>
2022-07-08 16:37:40 +0200

    users withdept action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\UsersWithDept;
 use App\Models\Company;
 use App\Models\User;
 use Carbon\Carbon;
@@ -9,47 +10,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    private Company $company;
 
-    public function __construct(Company $company)
+    public function index()
     {
-        $this->company = $company;
-    }
-
-    public function getUsers()
-    {
-        $users = $this->getUsersWithDept();
+        $deptAction = new UsersWithDept();
+        $users = $deptAction->execute();
 
         return response()->json(['users' => $users]);
-    }
-
-    public function getUsersWithDept($withOrdersForToday = false, $today = null)
-    {
-        $users = User::query()->where('company_id', $this->company->id)->where(function ($query) {
-            $query->has('orders')->orHas('payments');
-        })->with(['orders' => function ($query) use ($today) {
-            $query->whereNotNull('paid_by');
-            if ($today) {
-                $query->where('date', '>=', Carbon::now()->startOfDay());
-            }
-        }, 'payments'])
-            ->when($withOrdersForToday, function ($q) {
-                $q->whereHas('orders', function ($q2) {
-                    $q2->where('date', '>=', Carbon::now()->startOfDay());
-                });
-            })->get();
-        $users = self::calculateDept($users);
-        return $users;
-    }
-
-    public function calculateDept($users)
-    {
-        foreach ($users as $user) {
-            $user->orders_dept = $user->orders->sum('total');
-            $user->payments_sum = $user->payments->sum('total');
-            $user->dept = round($user->payments_sum - $user->orders_dept, 2);
-        }
-        $users->sortByDesc('dept');
-        return $users;
     }
 }
