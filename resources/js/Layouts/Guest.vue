66ae99d39976d7aa8896af284eb6d24b42eccabc
Jennis Vanhaeke <jennis@libaro.be>
2023-01-20 14:12:26 +0100

    ux login fix

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -4,10 +4,10 @@ import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-rainbow">
         <div>
             <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <BreezeApplicationLogo class="h-20 fill-current text-gray-500" />
             </Link>
         </div>
 
