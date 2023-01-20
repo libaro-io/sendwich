0c3faaafaab9cd763809e31e757bc65f444c9723
Koenraad <koenraad@libaro.be>
2023-01-20 16:52:00 +0100

    frontend page

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,5 +7,26 @@ use Inertia\Inertia;
 
 class CompanyController extends Controller
 {
+    public function show(){
+        $user = auth()->user() ;
+        $company = $user->company;
+        $users = $company->users;
 
+        return Inertia::render('Company',
+            [
+                'user' => $user,
+                'users' => $users,
+                'company' => $company,
+            ]);
+    }
+
+    public function inviteUser(Request $request){
+
+        $data = $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+        ]);
+
+        dd('passed');
+    }
 }
