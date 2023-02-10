78cd353c409dcc8372e4f8616b034179ae2da1d2
Koenraad <koenraad@libaro.be>
2023-02-10 16:18:46 +0100

    Add link on navbar

--- resources/views/emails/invitationMail.blade.php
+++ resources/views/emails/invitationMail.blade.php
@@ -1,8 +1,31 @@
-
-<div class="flex">
-    <p>Hallo {{$invitee->name}},</p>
+<style>
+    .wrapper {
+        font-family: sans-serif;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 500px;
+    }
+    .btn {
+        padding: 10px 20px;
+        font-size: 20px;
+        background-color: dodgerblue;
+        color: whitesmoke;
+        text-decoration: none;
+        border-radius: 20px;
+    }
+    .name {
+        text-transform: capitalize;
+    }
+</style>
+<div class="wrapper">
+    <h4>Hallo <span class="name">{{$invitee->name}}</span>{{$invitee->name}},</h4>
     <p>Je bent uitgenodigd voor de sendwich dienst .</p>
     <p>Bevestig je deelname </p>
-    <a href="{{$signedUrl}}">Ja ik doe mee !</a>
+    <a class="btn" href="{{$signedUrl}}">Ja ik doe mee !</a>
 </div>
+
+
+
 
