d244fff9e32cd81152a1689033862f97d93a3e18
Jennis Vanhaeke <jennis@libaro.be>
2023-04-11 10:15:52 +0200

    Features written down

--- resources/js/Components/frontend/Features.vue
+++ resources/js/Components/frontend/Features.vue
@@ -1,51 +1,37 @@
 <template>
-    <div class="relative bg-white">
+    <div class="relative bg-white px-50">
         <div class="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
-            <div class="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
+            <div class="lg:flex lg:w-1/3 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/3">
                 <div class="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0 px-6">
-                    <img class="absolute inset-0 h-full w-full bg-gray-50 object-cover" src="/images/runners.png"
-                         alt="">
+                    <img class="my-20 rounded-md bg-gray-50 object-cover" src="/images/runners.png" alt="">
                 </div>
             </div>
             <div class="px-6 lg:contents">
                 <div
                     class="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No more hassle on paying your colleagues back</h1>
+                    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No more hassle on paying back your colleagues</h1>
                     <p class="mt-6 text-xl leading-8 text-gray-700">Sendwich keeps track of the balance who ordered what</p>
                     <div class="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
-                        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper
-                            sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra
-                            tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed
-                            eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+<!--                        <p> The person with the lowest</p>-->
                         <ul role="list" class="mt-8 space-y-8 text-gray-600">
+
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20"
-                                     fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                          d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                                          clip-rule="evenodd"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-1 h-5 w-5 flex-none text-indigo-600">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+                                <span>The person with the lowest balance gets the order. </span>
                             </li>
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20"
-                                     fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd"
-                                          d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                                          clip-rule="evenodd"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-1 h-5 w-5 flex-none text-indigo-600">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+                                <span>However, everytime someone gets an order, the total value of that order is added to the balance of that person. So the next time someone needs to get the order.</span>
                             </li>
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20"
-                                     fill="currentColor" aria-hidden="true">
-                                    <path
-                                        d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"/>
-                                    <path fill-rule="evenodd"
-                                          d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
-                                          clip-rule="evenodd"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-1 h-5 w-5 flex-none text-indigo-600">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+                                <span>The balance keeps track on the balance of every person. So no one ever needs to send another money.</span>
                             </li>
                         </ul>
 
