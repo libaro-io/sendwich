06defeebc67a65b1d04dbafd5faa4c8f72749b3f
Jennis Vanhaeke <jennis@libaro.be>
2023-06-30 16:37:59 +0200

    Remove delete button on own user

--- resources/js/Pages/Company.vue
+++ resources/js/Pages/Company.vue
@@ -121,7 +121,7 @@ const deleteUser = (user) => {
                                                     <td>
                                                         <input type="checkbox" v-model="user.canEditCompany" class="checkbox" @change="togglePermission('edit-company', user)" />
                                                     </td>
-                                                    <td>
+                                                    <td v-if="$page.props.auth.user.id !== user.id">
                                                         <button class="btn btn-accent btn-circle btn-xs" @click="deleteUser(user)">
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                                         </button>
