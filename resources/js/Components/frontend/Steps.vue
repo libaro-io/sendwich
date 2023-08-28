5e91164741a2a591531fc3167b4a838954c33fc5
neemspees (Kim Bouchouaram) <kim@libaro.be>
2023-08-28 10:23:52 +0200

    Fix missing button text

--- resources/js/Components/frontend/Steps.vue
+++ resources/js/Components/frontend/Steps.vue
@@ -52,7 +52,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
                             <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">And stop wasting valuable time on getting lunch every day.</p>
                             <div class="mt-10 flex items-center justify-center gap-x-6">
                                 <a :href="route('company.register')" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
-                                    et started for free</a>
+                                    No, seriously... Get started for free</a>
                             </div>
                         </div>
                     </div>
