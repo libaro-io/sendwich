cd502bc15ba12313330434071755e124be1f83f3
AmauryDecru <amaury@libaro.be>
2024-04-12 15:35:49 +0200

    Fix block position

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -23,39 +23,40 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen bg-rainbow flex flex-col justify-between">
-            <nav class="nav ">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <BreezeApplicationLogo class="block h-9 w-auto" />
-                                </Link>
+            <div>
+                <nav class="nav ">
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="flex justify-between h-16">
+                            <div class="flex">
+                                <!-- Logo -->
+                                <div class="shrink-0 flex items-center">
+                                    <Link :href="route('dashboard')">
+                                        <BreezeApplicationLogo class="block h-9 w-auto" />
+                                    </Link>
+                                </div>
+
+                                <!-- Navigation Links -->
+                                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                    <BreezeNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                        Today
+                                    </BreezeNavLink>
+                                    <BreezeNavLink :href="route('history')" :active="route().current('history')">
+                                        History
+                                    </BreezeNavLink>
+                                    <BreezeNavLink v-if="can('edit-store')" :href="route('store.index')" :active="route().current('store.index')">
+                                        Menu
+                                    </BreezeNavLink>
+                                    <BreezeNavLink v-if="can('edit-company')" :href="route('company.show')" :active="route().current('company.show')">
+                                        Users
+                                    </BreezeNavLink>
+                                </div>
                             </div>
 
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <BreezeNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Today
-                                </BreezeNavLink>
-                                <BreezeNavLink :href="route('history')" :active="route().current('history')">
-                                    History
-                                </BreezeNavLink>
-                                <BreezeNavLink v-if="can('edit-store')" :href="route('store.index')" :active="route().current('store.index')">
-                                    Menu
-                                </BreezeNavLink>
-                                <BreezeNavLink v-if="can('edit-company')" :href="route('company.show')" :active="route().current('company.show')">
-                                    Users
-                                </BreezeNavLink>
-                            </div>
-                        </div>
-
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
-                                <BreezeDropdown align="right" width="48">
-                                    <template #trigger>
+                            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                <!-- Settings Dropdown -->
+                                <div class="ml-3 relative">
+                                    <BreezeDropdown align="right" width="48">
+                                        <template #trigger>
                                         <span class="inline-flex rounded-md">
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                                 {{ $page.props.auth.user.name }}
@@ -65,75 +66,77 @@ const showingNavigationDropdown = ref(false);
                                                 </svg>
                                             </button>
                                         </span>
-                                    </template>
+                                        </template>
 
-                                    <template #content>
-                                        <BreezeDropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </BreezeDropdownLink>
-                                    </template>
-                                </BreezeDropdown>
+                                        <template #content>
+                                            <BreezeDropdownLink :href="route('logout')" method="post" as="button">
+                                                Log Out
+                                            </BreezeDropdownLink>
+                                        </template>
+                                    </BreezeDropdown>
+                                </div>
+                            </div>
+
+                            <!-- Hamburger -->
+                            <div class="-mr-2 flex items-center sm:hidden">
+                                <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                        <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                        <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
-
-                        <!-- Hamburger -->
-                        <div class="-mr-2 flex items-center sm:hidden">
-                            <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                    <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
-                    <div class="pt-2 pb-3 space-y-1">
-                        <BreezeResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Today
-                        </BreezeResponsiveNavLink>
-                        <BreezeResponsiveNavLink :href="route('history')" :active="route().current('history')">
-                            History
-                        </BreezeResponsiveNavLink>
-                        <BreezeResponsiveNavLink v-if="can('edit-store')" :href="route('store.index')" :active="route().current('store.index')">
-                            Menu
-                        </BreezeResponsiveNavLink>
-                        <BreezeResponsiveNavLink v-if="can('edit-company')" :href="route('company.show')" :active="route().current('company.show')">
-                            Users
-                        </BreezeResponsiveNavLink>
                     </div>
 
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">{{ $page.props.auth.user.name }}</div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <BreezeResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
+                    <!-- Responsive Navigation Menu -->
+                    <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
+                        <div class="pt-2 pb-3 space-y-1">
+                            <BreezeResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                Today
+                            </BreezeResponsiveNavLink>
+                            <BreezeResponsiveNavLink :href="route('history')" :active="route().current('history')">
+                                History
+                            </BreezeResponsiveNavLink>
+                            <BreezeResponsiveNavLink v-if="can('edit-store')" :href="route('store.index')" :active="route().current('store.index')">
+                                Menu
+                            </BreezeResponsiveNavLink>
+                            <BreezeResponsiveNavLink v-if="can('edit-company')" :href="route('company.show')" :active="route().current('company.show')">
+                                Users
                             </BreezeResponsiveNavLink>
                         </div>
+
+                        <!-- Responsive Settings Options -->
+                        <div class="pt-4 pb-1 border-t border-gray-200">
+                            <div class="px-4">
+                                <div class="font-medium text-base text-gray-800">{{ $page.props.auth.user.name }}</div>
+                                <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+                            </div>
+
+                            <div class="mt-3 space-y-1">
+                                <BreezeResponsiveNavLink :href="route('logout')" method="post" as="button">
+                                    Log Out
+                                </BreezeResponsiveNavLink>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+                <!-- Page Heading -->
+                <header class="bg-white shadow" v-if="$slots.header">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <slot name="header" />
+                    </div>
+                </header>
+                <!-- Page Content -->
+                <main>
+                    <FlashMessages />
+                    <slot />
+                </main>
+            </div>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <FlashMessages />
-                <slot />
-            </main>
-            <Footer></Footer>
+            <div>
+                <Footer></Footer>
+            </div>
         </div>
     </div>
 </template>
