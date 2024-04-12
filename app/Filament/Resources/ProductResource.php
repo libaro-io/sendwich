0a1d623ea97187fbd9646beed506198fcf321caa
Robin <robin@libaro.be>
2024-04-12 16:25:33 +0200

    Add input fields for products

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -4,6 +4,11 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProductResource\Pages;
 use App\Models\Product;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -19,7 +24,19 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')->required(),
+                Textarea::make('description'),
+                TextInput::make('price')->numeric()->minValue(0)->step(0.01)->required(),
+                Select::make('company_id')->relationship('company', 'name')->required(),
+                Select::make('store_id')->relationship('store', 'name')->required(),
+
+                Repeater::make('options')->relationship('options')
+                    ->schema([
+                        TextInput::make('name')->required(),
+                        TextInput::make('price')->numeric()->minValue(0)->step(0.01)->required(),
+                        Checkbox::make('is_enabled_by_default')->label('Enabled by default'),
+                    ])
+                    ->columns(2)
             ]);
     }
 
